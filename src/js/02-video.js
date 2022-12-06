ff0860b00a57f37e4b43b8d94d0a810c4078a781
lassandra85 <lassandra85@gmail.com>
2022-12-06 12:11:56 +0100

    add2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 updatePlayer();
 
