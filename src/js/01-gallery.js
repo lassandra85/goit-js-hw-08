9afa29e30c6e48edddd9a153cf8d06a0f10417bf
lassandra85 <lassandra85@gmail.com>
2024-04-25 11:59:45 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,26 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, original, description }) => { return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>` }).join("");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src= '${preview}' alt="${description}" />
+            </a>
+        </li>`
+  )
+  .join('');
 
-galleryList.innerHTML = markup;
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', markup);
 
-galleryList = new SimpleLightbox(".gallery a", {
-  captions: true,
+const galleryShow = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
-  captionSelector: "img",
-  captionsData: "alt",
-  captionPosition: "bottom",
+  doubleTapZoom: 5,
 });
-
-
